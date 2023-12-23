7196a5241bfe6347d615da66dd992534dae861f0
eugeneduda <duda.evgen@gmail.com>
2023-12-23 16:57:09 +0200

    Add a SECRET_KEY env variable

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
     sudo apt update
     sudo apt install python3.12
-    python3.12 -m venv ./.env
+    python3.12 -m venv ./.venv
         
         sudo apt-get install python3.12-distutils
         curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
         python3.12 -m pip install opencv-python
         apt-get install python3.12-dev python3.12-venv
 
-    source .env/bin/activate
+    source .venv/bin/activate
     poetry install
--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk(zc&8f#pedw!2hqtqi^q2*mx08w9*ca(czkm)jd1eqy#mfm%'
+SECRET_KEY = getenv("SECRET_KEY", "test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
